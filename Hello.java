19dbe48d14bc663c47e52dbffdb4febb6e3f476d
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2023-05-03 18:32:40 -0400

    multiple languages

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello There");
+        System.out.println("Hola Mundo");
+        System.out.println("Bonjour People");
+        System.out.println("😀");
     }
 }
